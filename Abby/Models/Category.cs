7e4797fa2ca83342d4b32b16882876910d390354
Shailesh Pandavadra <pandavadra.shailesh@outlook.com>
2025-12-05 12:34:54 +0530

    Section 2 - Add Data Annotations

--- Abby/Models/Category.cs
+++ Abby/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace Abby.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Abby.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
