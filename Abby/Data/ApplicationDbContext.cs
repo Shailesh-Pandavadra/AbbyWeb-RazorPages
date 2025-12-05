da60aa03896cc2b92d64f32eca90385d5c647a05
Shailesh Pandavadra <pandavadra.shailesh@outlook.com>
2025-12-05 12:45:10 +0530

    Section 2 - Add ApplicationDbContext

---
+++ Abby/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Abby.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Abby.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+    }
+}
