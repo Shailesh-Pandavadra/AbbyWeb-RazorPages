40603c847ce06a846026d296ac53ecb610e251c3
Shailesh Pandavadra <pandavadra.shailesh@outlook.com>
2025-12-05 11:22:10 +0530

    Add project files.

---
+++ Abby/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Abby.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
